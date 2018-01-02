64223d6cd97282af257bc3bcf18534117401ef81
zhengyinsui <zhengyinsui@gmail.com>
2018-01-02 02:11:04 +0000

    app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
@@ -8,5 +9,22 @@ def index():
 
 if __name__ == '__main__':
   app.run(port=33507)
+
+
+def ticker(filename):
+
+key = [] # company name
+value = [] # ticker symbol
+
+with open(filename) as f:
+    lst = [line.rstrip() for line in f]
+
+    for line in range(0, len(lst), 2):
+        key.append(lst[line])
+
+    for line in range(1, len(lst), 2):
+        value.append(lst[line])
+
+dictionary = dict(zip(key, value))
 
 # comment, yin version
