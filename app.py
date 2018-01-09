4069cb92da1ea2bce88d76fedf4c3e7e67a7ff69
zhengyinsui <zhengyinsui@gmail.com>
2018-01-09 11:33:27 -0800

    update

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from bokeh.embed import components
 
 def make_plot(df_last_month, col_name, title):
 
-
     p = figure(title = '{} over Last Month'.format(title), x_axis_label = 'Date', y_axis_label = 'Price (Dollars)', x_axis_type = 'datetime')
     p.line(df_last_month['date'], df_last_month[col_name], color = 'blue')
     p.circle(df_last_month['date'], df_last_month[col_name], color = 'red')
@@ -18,26 +17,33 @@ def make_plot(df_last_month, col_name, title):
 
 
 app = Flask(__name__)
+@app.route('/', methods = ['GET'])
+def main():
+  # return redirect('/index')
+# @app.route('/index', methods = ['GET', 'POST'])
+# def index():
+    return render_template('index.html')
 
-@app.route('/', method = ['GET'])
-def index():
-  return render_template('index.html')
-
-@app.route('/graph',methods=['POST'])
+@app.route('/hellowing', methods=['GET', 'POST'])
 def graph():
-    tick_name = request.args['ticker_name']
+    tick_name = request.form['ticker']
     url = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json'
-    r = requets.get(url, {'ticker': tick_name,
-                          'date.gt': '2010-01-01',
-                          'opts.columns': 'date, close, adj_close',
-                          'api_key': 'wNAMU_EAudzbWW6zxay'})
+    r = requests.get(url, {'ticker': tick_name,
+    					  'date.gt': '2010-01-01',
+    					  'qopts.columns': 'date,close,adj_close',
+    					  'api_key': 'wNAMU_EAudzbWW6zxay_'})
     r_new = r.json()
+    # f = open('testing_output.txt','w')
+    # f.write('Ticker: %s\n'%tick_name)
+    # f.write('Age: %s\n\n'%r_new)
+    # f.close()
     r_list = r_new['datatable']['data']
     labels = ['date', 'close', 'adj_close']
     df = pd.DataFrame(r_list, columns = labels)
     df_last_month = df.iloc[-1:-22:-1, 0:3]
     df_last_month['date'] = pd.to_datetime(df_last_month['date'])
-    if request.args['closing'] == 'closing_price':
+
+    if request.form['features'] == 'close': #??
         return make_plot(df_last_month, 'close', 'Closing Stock Price for {}'.format(tick_name))
     else:
         return make_plot(df_last_month, 'adj_close', 'Adj. Closing Stock Price for {}'.format(tick_name))
@@ -51,6 +57,18 @@ def graph():
 
 if __name__ == '__main__':
     app.debug = True
-    port = int(os.environ.get('PORT', 5000))
-    # app.run(port=33507)
-    app.run(host = '0.0.0.0', port = port)
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(port=5000)
+    # app.run(host = '0.0.0.0', port = port)
+
+
+# @app.route('/')
+# def index():
+#   return render_template('index.html')
+#
+# @app.route('/about')
+# def about():
+#   return render_template('about.html')
+#
+# if __name__ == '__main__':
+#   app.run(port=33507)
